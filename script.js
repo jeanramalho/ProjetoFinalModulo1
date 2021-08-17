ca77926b18f4d42392715fc665db8cebbd02aa3e
Jean <jeanramalho.dev@gmail.com>
2021-08-17 16:31:10 -0300

    Fases concluidas e arquivo JS quase prontos

---
+++ script.js
@@ -0,0 +1,338 @@
+
+
+function lomal() {
+    location.href= "fases/lomal-fase1.html"
+    return
+}
+
+function rosaferd() {
+    location.href= "fases/rosaferd-fase1.html"
+    return
+}
+
+function chona() {
+    location.href= "fases/chona-fase1.html"
+    return
+}
+
+function jogarNovamente() {
+    location.href= "../index.html"
+}
+
+var testanto = "testando testando testando"
+
+
+    
+    
+
+
+
+
+
+function lomalF1() {
+
+    var respost = prompt("O que Lomal Girdle deve fazer?\n1 - Dar a volta.\n2 - Enfrentar Cavaleiro.")
+
+    if(respost == 1) {
+        location.href = "lomal-fase2.html"
+    } else if (respost == 2) {
+        location.href = "gameOver.html"
+        alert(`${testanto}`)
+        
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que Lomal Girdle deve fazer?\n1 - Dar a volta.\n2 - Enfrentar Cavaleiro.")
+
+        if(respost == 1) {
+            location.href = "lomal-fase2.html"
+        } else if (respost == 2) {
+            
+            location.href = "gameOver.html" 
+
+        }
+
+    }
+                
+    return 
+}
+
+
+function lomalF2() {
+    
+    var respost = prompt("O que Lomal Girdle deve fazer?\n1 - Subir em uma arvore.\n2 - Abaixar e rastejar.")
+
+    if(respost == 2) {
+        location.href = "lomal-fase3.html"
+    } else if (respost == 1) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que Lomal Girdle deve fazer?\n1 - Subir em uma arvore.\n2 - Abaixar e rastejar.")
+
+        if(respost == 2) {
+            location.href = "lomal-fase3.html"
+        } else if (respost == 1) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+function lomalF3() {
+    
+    var respost = prompt("O que Lomal Girdle deve fazer?\n1 - Pegar o Anel.\n2 - Esperar seus amigos.")
+
+    if(respost == 2) {
+        location.href = "lomal-fase4.html"
+    } else if (respost == 1) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que Lomal Girdle deve fazer?\n1 - Pegar o Anel.\n2 - Esperar seus amigos.")
+
+        if(respost == 2) {
+            location.href = "lomal-fase4.html"
+        } else if (respost == 1) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+function lomalF4() {
+    
+    var respost = prompt("O que os Hobbits devem fazer?\n1 - Pegar o Anel.\n2 - Correr e fugir.")
+
+    if(respost == 1) {
+        location.href = "zerou.html"
+    } else if (respost == 2) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que os Hobbits devem fazer?\n1 - Pegar o Anel.\n2 - Correr e Fugir.")
+
+        if(respost == 1) {
+            location.href = "zerou.html"
+        } else if (respost == 2) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+function rosaferdF1() {
+    
+    var respost = prompt("O que Rosaferd Hand deve fazer?\n1 - Passar pela ponte.\n2 - Agarrar-se ao tronco.")
+
+    if(respost == 2) {
+        location.href = "rosaferd-fase2.html"
+    } else if (respost == 1) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que Rosaferd Hand deve fazer?\n1 - Passar pela ponte.\n2 - Agarrar-se ao tronco.")
+
+        if(respost == 2) {
+            location.href = "rosaferd-fase2.html"
+        } else if (respost == 1) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+function rosaferdF2() {
+    
+    var respost = prompt("O que Rosaferd Hand deve fazer?\n1 - Passar pelo Warg.\n2 - Passar pela Eagle.")
+
+    if(respost == 2) {
+        location.href = "rosaferd-fase3.html"
+    } else if (respost == 1) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que Rosaferd Hand deve fazer?\n1 - Passar pelo Warg.\n2 - Passar pela Eagle.")
+
+        if(respost == 2) {
+            location.href = "rosaferd-fase3.html"
+        } else if (respost == 1) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+
+function rosaferdF3() {
+    
+    var respost = prompt("O que Rosaferd Hand deve fazer?\n1 - Esperar Chona Ville.\n2 - Pegar o Anel sozinho.")
+
+    if(respost == 1) {
+        location.href = "rosaferd-fase4.html"
+    } else if (respost == 2) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que Rosaferd Hand deve fazer?\n1 - Esperar Chona Ville.\n2 - Pegar o Anel sozinho.")
+
+        if(respost == 1) {
+            location.href = "rosaferd-fase4.html"
+        } else if (respost == 2) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+
+function rosaferdF4() {
+    
+    var respost = prompt("O que os Hobbits devem fazer?\n1 - Pegar o Anel.\n2 - Correr e fugir.")
+
+    if(respost == 1) {
+        location.href = "zerou.html"
+    } else if (respost == 2) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que os Hobbits devem fazer?\n1 - Pegar o Anel.\n2 - Correr e Fugir.")
+
+        if(respost == 1) {
+            location.href = "zerou.html"
+        } else if (respost == 2) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+
+function chonaF1() {
+    
+    var respost = prompt("O que Chona Ville deve fazer?\n1 - Correr.\n2 - Enfrentar a Árvore Ent.")
+
+    if(respost == 1) {
+        location.href = "chona-fase2.html"
+    } else if (respost == 2) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que Chona Ville deve fazer?\n1 - Correr.\n2 - Enfrentar a Árvore Ent.")
+
+        if(respost == 1) {
+            location.href = "chona-fase2.html"
+        } else if (respost == 2) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+
+function chonaF2() {
+    
+    var respost = prompt("O que Chona Ville deve fazer?\n1 - Entrar no Templo.\n2 - Traduzir a inscrição.")
+
+    if(respost == 2) {
+        location.href = "chona-fase3.html"
+    } else if (respost == 1) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que Chona Ville deve fazer?\n1 - Entrar no Templo.\n2 - Traduzir a inscrição.")
+
+        if(respost == 2) {
+            location.href = "chona-fase3.html"
+        } else if (respost == 1) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
+
+function chonaF3() {
+    
+    var respost = prompt("O que os Hobbits devem fazer?\n1 - Pegar o Anel.\n2 - Correr e fugir.")
+
+    if(respost == 1) {
+        location.href = "zerou.html"
+    } else if (respost == 2) {
+        location.href = "gameOver.html"
+    }
+
+    while (respost != 1 && respost != 2) {
+
+        alert("Opção inválida! Digite 1 ou 2 para responder!")
+
+        respost = prompt("O que os Hobbits devem fazer?\n1 - Pegar o Anel.\n2 - Correr e Fugir.")
+
+        if(respost == 1) {
+            location.href = "zerou.html"
+        } else if (respost == 2) {
+            location.href = "gameOver.html"
+        }
+
+    }
+                
+    return 
+}
+
